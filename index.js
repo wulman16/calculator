05a422336d64a4708144ae44de378da200c6c039
wulman16 <will.ulman@gmail.com>
2019-03-21 07:59:51 -0400

    implement basic functionality

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('form').addEventListener('click', populateExpression)
+})
+
+function populateExpression(e) {
+  if(e.target && e.target.nodeName === 'INPUT' && e.target.className === 'expression-input') {
+    const expression = document.getElementById('expression')
+    const existingValue = expression.value
+    expression.value = existingValue + e.target.value
+  } else if(e.target.className === 'clear') {
+    const expression = document.getElementById('expression')
+    expression.value = ''
+  } else if(e.target.className === 'evaluate') {
+    const expression = document.getElementById('expression')
+    try {
+      eval(expression.value)
+    }
+    catch(err) {
+      expression.value = 'ERROR'
+    }
+  }
+}
